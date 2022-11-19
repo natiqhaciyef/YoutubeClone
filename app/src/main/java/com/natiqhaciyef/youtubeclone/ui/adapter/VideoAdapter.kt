ab4cf374743fbdc7be87880ca4b21344eb5bcfe4
Natiq <natiq00h2272@gmail.com>
2022-11-19 21:32:39 +0400

    Data binding integration 2 (recycler view adapting)

--- app/src/main/java/com/natiqhaciyef/youtubeclone/ui/adapter/VideoAdapter.kt
+++ app/src/main/java/com/natiqhaciyef/youtubeclone/ui/adapter/VideoAdapter.kt
@@ -3,30 +3,47 @@ package com.natiqhaciyef.youtubeclone.ui.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.natiqhaciyef.youtubeclone.R
 import com.natiqhaciyef.youtubeclone.data.model.YoutubeVideo
 import com.natiqhaciyef.youtubeclone.databinding.RecyclerVideosBinding
 
-class VideoAdapter(private val mContext: Context, val list: List<YoutubeVideo>): RecyclerView.Adapter<VideoAdapter.VideoHolder>(){
+class VideoAdapter(private val mContext: Context, val list: List<YoutubeVideo>) :
+    RecyclerView.Adapter<VideoAdapter.VideoHolder>() {
 
-    inner class VideoHolder(val binding: RecyclerVideosBinding): RecyclerView.ViewHolder(binding.root)
+    inner class VideoHolder(val binding: RecyclerVideosBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoHolder {
-        val binding = RecyclerVideosBinding.inflate(LayoutInflater.from(mContext), parent, false)
+        val binding: RecyclerVideosBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(mContext),
+            R.layout.recycler_videos,
+            parent,
+            false
+        )
         return VideoHolder(binding)
     }
 
     override fun onBindViewHolder(holder: VideoHolder, position: Int) {
         val video = list[position]
         val itemView = holder.binding
+        itemView.video = video
 
-        itemView.videoImageView.setImageResource(mContext.resources.getIdentifier(video.videoImage,"drawable",mContext.packageName))
-        itemView.userProfileImageView.setImageResource(mContext.resources.getIdentifier(video.channelInfo.image,"drawable",mContext.packageName))
-
-        itemView.videoInfoTextView.text = "${video.channelInfo.name} - ${video.views} - ${video.videoPostTime}"
-        itemView.videoTitleTextView.text = "${video.title}"
-        itemView.videoTimeTextView.text = "${video.videoTimeLong}"
-
+        itemView.videoImageView.setImageResource(
+            mContext.resources.getIdentifier(
+                video.videoImage,
+                "drawable",
+                mContext.packageName
+            )
+        )
+        itemView.userProfileImageView.setImageResource(
+            mContext.resources.getIdentifier(
+                video.channelInfo.image,
+                "drawable",
+                mContext.packageName
+            )
+        )
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/natiqhaciyef/youtubeclone/ui/view/ShortsFragment.kt
+++ app/src/main/java/com/natiqhaciyef/youtubeclone/ui/view/ShortsFragment.kt
@@ -5,16 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.natiqhaciyef.youtubeclone.R
 import com.natiqhaciyef.youtubeclone.databinding.FragmentShortsBinding
 
 
 class ShortsFragment : Fragment() {
     private lateinit var binding: FragmentShortsBinding
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
-        binding = FragmentShortsBinding.inflate(inflater, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shorts ,container, false)
 
 
         return binding.root
